dac15e594528eb2ddc5527afa4c867a9ac969890
rakesh kumar <rakeshmaurya262@gmail.com>
2018-09-18 15:10:13 +0530

    chnages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,3 @@
 //= require_tree ./js
 
 
-$( document ).ready(function() {
-    $('ul.nav li.dropdown').hover(function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-      }, function() {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-      });
-
-});
